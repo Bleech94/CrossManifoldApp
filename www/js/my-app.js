1624c3a9e9b608a1448b98162da2258c9a651b0e
Brandon Leech <bleech94@hotmail.com>
2016-10-06 12:49:50 -0700

    Improve pubnub stuff, fix main page format, fix code format and cleanup

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,7 +1,10 @@
+/*
+* INITIALIZE
+*/
 var isMaterial = Framework7.prototype.device.ios === false;
 var isIos = Framework7.prototype.device.ios === true;
 
-var CMID = "Test"; // Cross Manifold ID. TODO get from login
+var CMID = ""; // Cross Manifold ID.
 var pubnubUpdateChannel = "CM_Update_" + CMID; // This channel is for updates from the Pi that will be displayed on the app.
 var pubnubCommandChannel = "CM_Command_" + CMID; // This channel is for commands from the app to the Pi to change the thermostats.
 
@@ -16,7 +19,7 @@ Template7.global = {
 
 var $$ = Dom7; // Custom DOM library, almost the exact same as jQuery
 
-// Rearrange some div classes to match android and iOS
+// Rearrange some div classes to match android and iOS. TODO: Check if working.
 if (!isIos) {
     $$('.pages.navbar-through').removeClass('navbar-through').addClass('navbar-fixed'); // Change class
     $$('.view .navbar').prependTo('.view .page'); // And move Navbar into Page
@@ -26,14 +29,14 @@ if (!isIos) {
 var myApp = new Framework7({
     material: isMaterial ? true : false,
     precompileTemplates: true,
-    template7Pages: true
+    template7Pages: true // TODO: Is this used?
 });
 
 // Add view
 var mainView = myApp.addView('.view-main', {
     // Because we want to use dynamic navbar, we need to enable it for this view:
     dynamicNavbar: true,
-    domCche: true
+    domCache: true // TODO: Is this used?
 });
 
 // TODO: Switch back to 'deviceready' event after testing
@@ -46,7 +49,7 @@ var mainView = myApp.addView('.view-main', {
 });*/
 
 /* TODO: remove after testing */
-if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
+if (navigator.userAgent.match(/(iPhone|Android)/)) {
     document.addEventListener("deviceready", onDeviceReady, false);
 } else {
     onDeviceReady();
@@ -57,10 +60,28 @@ function onDeviceReady() {
 }
 /* remove after testing */
 
+// Initialize Pubnub TODO: Ensure this is only ever called once?
+var pubnub = PUBNUB.init({
+    publish_key: 'pub-c-3082c989-10c9-4690-b759-2b7f56c733e7',
+    subscribe_key: 'sub-c-88a5713a-8040-11e6-920d-02ee2ddab7fe',
+    error: function (error) {
+      console.log('Error: ' + error);
+    }
+})
+
+
 
 /*
 * NAVIGATION
 */
+// Login: Check if the channel is live by checking the update history. If there is a message than the corresponding Pi is active.
+$$(document).on('click', '.login-button', function() {
+  console.log("Login clicked");
+  index=1;
+  pubnubLogin();
+})
+
+// Settings
 $$(document).on('click', '.navbar .settings-button', function() {
   // var settings = JSON.parse(localStorage.getItem('settings')); // TODO
   index = 1;
@@ -71,6 +92,7 @@ $$(document).on('click', '.navbar .settings-button', function() {
   });
 });
 
+// Logout
 $$(document).on('click', '.logout-button', function() {
   mainView.router.load({
     template: myApp.templates.login
@@ -80,14 +102,43 @@ $$(document).on('click', '.logout-button', function() {
   pubnubCommndChannel = "";
 })
 
+
+
 /*
 * CONTROL
 */
+// Send command to the Pi with the desired temperatures.
+$$(document).on('click', '.apply-button', function() {
+  console.log("Apply clicked.");
+
+  // Loop through all desired temps and push the cleaned numbers to an array.
+  var desiredTempArray = [];
+  $$(".desired-temp").each(function() {
+    desiredTempArray.push(parseInt($$(this).text().replace("°", "")));
+  });
+
+  pubnubPublishCommand(desiredTempArray);
+  console.log("Command sent.");
+
+  index = 1;
+
+  // Loop through all desired temps and push the cleaned numbers to an array.
+  var currentTempArray = [];
+  $$(".current-temp").each(function() {
+    console.log(".current-temp");
+    currentTempArray.push(parseInt($$(this).text().replace("°", "")));
+  });
+  pubnubPublishUpdate(currentTempArray, desiredTempArray);
+  console.log("Update sent.");
+})
+
+// Increment corresponding desired temperature.
 $$(document).on('click', '.increment', function() {
   var val = parseInt($$(this).prev().text().replace("°", "")) + 1;
   $$(this).prev().text(val + '°');
 })
 
+// Decrement corresponding desired temperature.
 $$(document).on('click', '.decrement', function() {
   var val = parseInt($$(this).next().text().replace("°", "")) - 1;
   $$(this).next().text(val + '°');
@@ -95,7 +146,6 @@ $$(document).on('click', '.decrement', function() {
 
 // Auto switch input box when full.
 $$(".CMID-input").keyup(function() {
-  console.log($$(this).val().length);
   if($$(this).val().length == 4) {
     $$(this).next().focus();
   }
@@ -106,40 +156,4 @@ $$('.CMID-input').keydown(function (e){
     if(e.keyCode == 13){
         pubnubLogin();
     }
-})
-
-
-
-/*
-* PUBNUB
-*/
-// TODO: Ensure this is only ever called once? Just need to make a global bool.
-var pubnub = PUBNUB.init({
-    publish_key: 'pub-c-3082c989-10c9-4690-b759-2b7f56c733e7',
-    subscribe_key: 'sub-c-88a5713a-8040-11e6-920d-02ee2ddab7fe',
-    error: function (error) {
-      console.log('Error: ' + error);
-    }
-})
-
-// Check if the channel is live by checking the update history.
-// If the Pi has ever sent an update to the channel that corresponds to the CMID then the user can "login" to this channel.
-$$(document).on('click', '.login-button', function() {
-  console.log("Login clicked");
-  index=1;
-  pubnubLogin();
-})
-
-// TODO
-$$(document).on('click', '.apply-button', function() {
-  console.log("Apply clicked.");
-
-  // Loop through all desired temps and push a the cleaned numbers to an array.
-  var desiredTempArray = [];
-  $$(".desired-temp").each(function() {
-      desiredTempArray.push(parseInt($$(this).text().replace("°", "")));
-  });
-
-  pubnubPublishCommand(desiredTempArray);
-  alert("Desired Temperatures Updated.");
 })
--- www/js/my-pubnub.js
+++ www/js/my-pubnub.js
@@ -3,30 +3,30 @@ var lastUpdateJSON;
 /*
 * Functions
 */
-// TODO: Fix this function. If there is no history, don't allow login. If there is some history then login is okay.
-// Should return true or false
+// "Logging in" is just subscribing to an active Pubnub channel. Each Pi automatically connects to a unique channel.
+// When the Pi starts it immediately sends an update. A phone can connect to any channel that has received an update.
 function pubnubLogin() {
   console.log("Attempting to login...");
 
-  // Update CMID and channel info.
+  // Get CMID from input fields and set channel names.
   CMID = $$('#CMID-input1').val() + $$('#CMID-input2').val() + $$('#CMID-input3').val() + $$('#CMID-input4').val();
-
-  console.log(CMID);
   pubnubUpdateChannel = "CM_Update_" + CMID;
   pubnubCommandChannel = "CM_Command_" + CMID;
 
   // Check if history is empty. If it is, alert() steps to try and fix it, otherwise login to main page.
   pubnub.history({
     channel: pubnubUpdateChannel,
+    count: 1,
     callback: function(m){
-        lastUpdateJSON = m[0][0]; // m[0] is a message array, m[0][0] is the only message we requested as a JSON object.
+        lastUpdateJSON = m[0][0]; // m[0] is a message array, m[0][0] is the single message we requested as a JSON object.
         console.log(JSON.stringify(lastUpdateJSON));
         if(lastUpdateJSON) { // If there is any history in pubnubUpdateChannel this value is true.
           pubnubSubscribeToUpdates();
           mainView.router.load({
             template: myApp.templates.main,
             animatePages:true,
-            context: lastUpdateJSON
+            context: lastUpdateJSON,
+            reload: true
           });
         } else {
           alert("The Cross Manifold ID you entered did not match any active Cross Manifolds. Please try again."); // TODO: Add more information.
@@ -37,8 +37,7 @@ function pubnubLogin() {
           $$('#CMID-input4').val("");
           $$('#CMID-input1').focus();
         }
-    },
-    count: 1
+    }
   });
 }
 
@@ -47,13 +46,18 @@ function pubnubSubscribeToUpdates() {
   pubnub.subscribe({
     channel: pubnubUpdateChannel,
     message: function(m) {
-      console.log("Message received: " + JSON.stringify(m)) // TODO update current thermostat readings on main page when message recieved
+      lastUpdateJSON = m; // m is the message we received.
+      console.log(JSON.stringify(lastUpdateJSON));
+      mainView.router.load({
+        template: myApp.templates.main,
+        animatePages:false,
+        context: lastUpdateJSON,
+        reload:true
+      });
     },
     connect: function(m) {console.log("Connected: " + m)},
     disconnect: function(m) {
       console.log("Disconnected: " + m);
-      pubnubSubscribeToUpdates(); // TODO remove this?
-      console.log("Resubscribed.");
     },
     reconnect: function(m) {console.log("Reconnected: " + m)},
     error: function(m) {console.log("Network Error: " + m[0] + ' ' + m[1])},
@@ -62,9 +66,35 @@ function pubnubSubscribeToUpdates() {
 }
 
 function pubnubPublishCommand(tempArray) {
+  // Publish the array of desired temperatures selected to the channel that the Pi is subscribed to.
   pubnub.publish({
     channel: pubnubCommandChannel,
     message: tempArray,
+    callback: function(m){
+        console.log(m)
+    }
+  })
+}
+
+// This function is only used when Apply is pressed. It keeps the app and Pi sync'd.
+// Without this you can Apply, restart the app and your changes will not be shown (but the Pi will have received them).
+function pubnubPublishUpdate(currentArray, desiredArray) {
+  var thermostatReadingArray = [];
+  console.log(currentArray.length);
+  // Loop through the current and desired temps for each thermostat, construct reading objects and push them to an array
+  for(var i = 0; i < currentArray.length; i++) {
+    var reading = {
+      "current": currentArray[i],
+      "desired": desiredArray[i]
+    }
+    thermostatReadingArray.push(reading);
+  }
+  var newUpdateJSON = {"thermostatReading":thermostatReadingArray}
+  lastUpdateJSON = newUpdateJSON;
+
+  pubnub.publish({
+    channel: pubnubUpdateChannel,
+    message: newUpdateJSON,
     callback: function(m){
         console.log(m)
     }
